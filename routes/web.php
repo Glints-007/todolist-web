eb58190a6501c03eece93e88dcf94231bbfacd1f
Raendy Andhika El-Vicar <raendy.elvicar@gmail.com>
2021-09-07 20:36:47 +0700

    trying to solve conflict

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Http\Controllers\TodoController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
